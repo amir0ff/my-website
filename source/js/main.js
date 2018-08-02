e0105193d3bbd3b9de5305dc40bc76fecd9eda2f
Amir Off <ameer157@gmail.com>
2018-08-03 02:27:18 +0300

    Fix deprecated jQuery syntax

--- source/js/main.js
+++ source/js/main.js
@@ -30,7 +30,7 @@ function isElementInViewport (el) {
 
 // function to smooth scroll to elements on link clicks
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*=\\#]:not([href=\\#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
 
