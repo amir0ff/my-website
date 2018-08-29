f5ac2f10ad82b06fef564127cbe8f4749c4009a7
Amir Off <ameer157@gmail.com>
2018-08-29 22:10:35 +0300

    Update portfolio

--- source/js/main.js
+++ source/js/main.js
@@ -77,7 +77,7 @@ $(document).ready(function () {
 
     function addRepos(data) {
         $.each(data.data, (index, repos) => {
-            let repo = $('<div class="card"><div class="card-body"><span class="badge badge-pill badge-info">' + repos.language + '</span><a href=" ' + repos.html_url +' " target="_blank"><div class="card-header text-left"><h5>' + repos.name + '</h5></a></div><p class="card-text text-left small">' + repos.description + '</p></div></div>');
+            let repo = $('<div class="card"><div class="card-body"><span class="badge badge-pill badge-info">' + repos.language + '</span><a href=" ' + repos.html_url +' " target="_blank"><div class="card-header text-left"><h5>' + repos.name + '</h5></div></a><p class="card-text text-left small">' + repos.description + '</p></div></div>');
             repo.prependTo('#repositories');
         })
     }
