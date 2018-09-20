f4fa1cb7d0fffcf78de0e6f20b98071dc0505541
Amir Off <ameer157@gmail.com>
2018-09-20 12:05:32 +0300

    Fix variable names

--- source/js/main.js
+++ source/js/main.js
@@ -90,11 +90,11 @@ $(document).ready(function () {
 
     function addBlogPosts(data) {
         let filteredPosts = data.filter(item => item.categories.length > 0);
-        $.each(filteredPosts, (index, post) => {
-            const momentDate = moment(post.pubDate).format('MMM D, YYYY');
-            const filteredDescription = post.content.replace(/(<img[^>]+?>|<img>|<\/img>|<p>|<\/p>|<blockquote>|<\/blockquote>|<a[^>]*>|<strong>|<\/strong>)/img, "").substring(0, 135);
-            const repo = $('<div class="card"><div class="card-body"><span class="date"><i class="fas fa-clock"></i>' + momentDate + '</span><a href="' + post.link + '" target="_blank"><div class="cover"><i class="fa fa-book-open fa-4x"></i><img src="' + post.thumbnail + '"></div><h5>' + post.title + '</h5></a><div class="card-text">' + filteredDescription + '</div></div></div>');
-            repo.prependTo('#feeds');
+        $.each(filteredPosts, (index, posts) => {
+            const momentDate = moment(posts.pubDate).format('MMM D, YYYY');
+            const formattedText = posts.content.replace(/(<img[^>]+?>|<img>|<\/img>|<p>|<\/p>|<blockquote>|<\/blockquote>|<a[^>]*>|<strong>|<\/strong>)/img, "").substring(0, 135);
+            const post = $('<div class="card"><div class="card-body"><span class="date"><i class="fas fa-clock"></i>' + momentDate + '</span><a href="' + posts.link + '" target="_blank"><div class="cover"><i class="fa fa-book-open fa-4x"></i><img src="' + posts.thumbnail + '"></div><h5>' + posts.title + '</h5></a><div class="card-text">' + formattedText + '</div></div></div>');
+            post.prependTo('#feeds');
         })
     }
 
