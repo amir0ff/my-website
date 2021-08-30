9edfe047bb4a340e85b5ad74e82fe406687b38e0
Amir <ameer157@gmail.com>
2021-08-30 20:13:28 +0300

    refactor: cleanup main js and php send mail scripts

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
 let currentScroll = 0;
 let canScroll = true; // if false, blocks browser from scrolling - used for non-touch devices and desktops (touch devices ignore this)
-let smallDevice = window.matchMedia("only screen and (max-width: 760px)");
+let smallDevice = window.matchMedia('only screen and (max-width: 760px)');
 /*let portfolio1Shown = false;*/
 let portfolio2Shown = false;
 let portfolio3Shown = false;
@@ -8,7 +8,7 @@ let portfolio3Shown = false;
 function isElementInViewport(el) {
 
   //special bonus for those using jQuery
-  if (typeof jQuery === "function" && el instanceof jQuery) {
+  if (typeof jQuery === 'function' && el instanceof jQuery) {
     el = el[0];
   }
 
@@ -24,11 +24,11 @@ function isElementInViewport(el) {
 
 // function to smooth scroll to elements on link clicks
 $(function() {
-  $("a[href*=\\#]:not([href=\\#])").click(function() {
-    if (location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname) {
+  $('a[href*=\\#]:not([href=\\#])').click(() => {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
       let target = $(this.hash);
 
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
 
         canScroll = false;
@@ -36,22 +36,22 @@ $(function() {
         // determine where we'll scroll
         let newScroll = 0;
         if (!(smallDevice.matches)) {
-          newScroll = target.offset().top - $(".navbar").height();
+          newScroll = target.offset().top - $('.navbar').height();
           // for desktops, we play with the nav bar padding
-          if (newScroll < ($("#profile").offset().top - $(".navbar").height() - 150)) {
-            $(".navbar").animate({ "padding-top": "30px" }, 500);
-            $(".navbar").css("background", "none");
-            $(".navbar").css("border-color", "transparent");
+          if (newScroll < ($('#profile').offset().top - $('.navbar').height() - 150)) {
+            $('.navbar').animate({ 'padding-top': '30px' }, 500);
+            $('.navbar').css('background', 'none');
+            $('.navbar').css('border-color', 'transparent');
           } else {
-            $(".navbar").animate({ "padding-top": "0px" }, 500);
-            $(".navbar").css("background", "#0D0D0D");
-            $(".navbar").css("border-color", "#0A0A0A");
+            $('.navbar').animate({ 'padding-top': '0px' }, 500);
+            $('.navbar').css('background', '#0D0D0D');
+            $('.navbar').css('border-color', '#0A0A0A');
           }
         } else {
-          newScroll = target.offset().top - $(".navbar-collapse:visible").height();
+          newScroll = target.offset().top - $('.navbar-collapse:visible').height();
         }
 
-        $("html,body").animate({
+        $('html,body').animate({
           scrollTop: newScroll + 30,
         }, 500, function() {
           canScroll = true;
@@ -62,65 +62,57 @@ $(function() {
   });
 });
 
-$(document).ready(function() {
+$(document).ready(() => {
 
   (function getGitHubRepos() {
-    const reposURL = "https://api.github.com/users/amiroff157/repos?callback=allow";
-    $.getJSON(reposURL + "&callback=?", (data) => {
-      addGitHubRepos(data.data);
+    const reposURL = 'https://api.github.com/users/amiroff157/repos?callback=allow';
+    $.getJSON(reposURL + '&callback=?', (data) => {
+      $.each(data.data, (index, repos) => {
+        const repo = $('<div class="card"><div class="card-body"><span class="badge"><span class="repo-lang-color ' + repos.language.toLocaleLowerCase() + '"></span> ' + repos.language + '</span><a href=" ' + repos.html_url + ' " target="_blank"><div class="card-header text-left"><h5>' + repos.name + '</h5></div></a><p class="card-text text-left small">' + repos.description + '</p></div></div>');
+        repo.prependTo('#repositories');
+      });
     });
   })();
-
-  function addGitHubRepos(data) {
-    $.each(data, (index, repos) => {
-      const repo = $("<div class=\"card\"><div class=\"card-body\"><span class=\"badge\"><span class=\"repo-lang-color " + repos.language.toLocaleLowerCase() + "\"></span> " + repos.language + "</span><a href=\" " + repos.html_url + " \" target=\"_blank\"><div class=\"card-header text-left\"><h5>" + repos.name + "</h5></div></a><p class=\"card-text text-left small\">" + repos.description + "</p></div></div>");
-      repo.prependTo("#repositories");
-    });
-  }
 
 
   (function getBlogPosts() {
-    let postsURL = "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40amiroff157&api_key=pdu4dawprzidibowy69tc3yiaxl6v0hpzmuxiook&order_by=pubDate&order_dir=asc&count=14";
+    let postsURL = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40amiroff157&api_key=pdu4dawprzidibowy69tc3yiaxl6v0hpzmuxiook&order_by=pubDate&order_dir=asc&count=14';
     $.getJSON(postsURL, (data) => {
-      addBlogPosts(data.items);
+      let filteredPosts = data.items.filter(item => item.categories.length > 0);
+      $.each(filteredPosts, (index, posts) => {
+        const momentDate = moment(posts.pubDate).format('MMM D, YYYY');
+        let formattedText = posts.content.replace(/(<img[^>]+?>|<img>|<\/img>|<p>|<\/p>|<h4>|<\/h4>|<blockquote>|<\/blockquote>|<figure>|<\/figure>|<figcaption>|<\/figcaption>|<a[^>]*>|<strong>|<\/strong>)/img, '').substring(0, 130);
+        formattedText += '...';
+        let postCoverImg = posts.thumbnail;
+        const post = $('<div class="card"><div class="card-body"><span class="date"><i class="fas fa-clock"></i>' + momentDate + '</span><a href="' + posts.link + '" target="_blank"><div class="cover" style="height:170px;background: url(' + postCoverImg + ') no-repeat center center;background-size: cover;"><i class="fa fa-book-open fa-4x"></i></div><h5>' + posts.title + '</h5></a><div class="card-text">' + formattedText + '</div></div></div>');
+        post.prependTo('#feeds');
+      });
     });
   })();
 
-  function addBlogPosts(data) {
-    let filteredPosts = data.filter(item => item.categories.length > 0);
-    $.each(filteredPosts, (index, posts) => {
-      const momentDate = moment(posts.pubDate).format("MMM D, YYYY");
-      let formattedText = posts.content.replace(/(<img[^>]+?>|<img>|<\/img>|<p>|<\/p>|<h4>|<\/h4>|<blockquote>|<\/blockquote>|<figure>|<\/figure>|<figcaption>|<\/figcaption>|<a[^>]*>|<strong>|<\/strong>)/img, "").substring(0, 130);
-      formattedText += "...";
-      let postCoverImg = "'" + posts.thumbnail + "'";
-      const post = $("<div class=\"card\"><div class=\"card-body\"><span class=\"date\"><i class=\"fas fa-clock\"></i>" + momentDate + "</span><a href=\"" + posts.link + "\" target=\"_blank\"><div class=\"cover\" style=\"height:170px;background: url(" + postCoverImg + ") no-repeat center center;background-size: cover;\"><i class=\"fa fa-book-open fa-4x\"></i></div><h5>" + posts.title + "</h5></a><div class=\"card-text\">" + formattedText + "</div></div></div>");
-      post.prependTo("#feeds");
-    });
-  }
 
+  $('#submit').click((event) => {
+    event.preventDefault();
 
-  $("#submit").click(function(e) {
-    e.preventDefault();
+    $('.error').hide();
 
-    $(".error").hide();
+    let sender = $('#name').val();
+    let email = $('#email').val();
+    let message = $('#message').val();
 
-    let theName = $("#name").val();
-    let theEmail = $("#email").val();
-    let theMessage = $("#message").val();
-
-    if (theName.length === 0) {
-      $("#nameError").fadeIn("slow");
-    } else if (theEmail.length === 0) {
-      $("#emailError").fadeIn("slow");
-    } else if (theMessage.length === 0) {
-      $("#messageError").fadeIn("slow");
+    if (sender.length === 0) {
+      $('#nameError').fadeIn('slow');
+    } else if (email.length === 0) {
+      $('#emailError').fadeIn('slow');
+    } else if (message.length === 0) {
+      $('#messageError').fadeIn('slow');
     } else {
-      $("#spinner").fadeIn().css("display", "inline-block");
-      $.post("php/send.php", { name: theName, email: theEmail, message: theMessage })
-        .done(function(data) {
+      $('#spinner').fadeIn().css('display', 'inline-block');
+      $.post('php/send.php', { name: sender, email: email, message: message })
+        .done(() => {
 
-          $("#form-container").fadeOut("slow", function() {
-            $("#success").fadeIn();
+          $('#form-container').fadeOut('slow', () => {
+            $('#success').fadeIn();
           });
 
         });
@@ -129,16 +121,16 @@ $(document).ready(function() {
   });
 
   // for collapsable nav bar, hide the menu when a link is clicked
-  $(".navbar-collapse ul li a").click(function() {
-    $(".navbar-toggle:visible").click();
+  $('.navbar-collapse ul li a').click(() => {
+    $('.navbar-toggle:visible').click();
   });
 
   // don't let user scroll if we are auto-scrolling
-  $("body").on({
-    "mousewheel": function(e) {
+  $('body').on({
+    'mousewheel': (event) => {
       if (!canScroll) {
-        e.preventDefault();
-        e.stopPropagation();
+        event.preventDefault();
+        event.stopPropagation();
         return false;
       }
     },
@@ -150,14 +142,14 @@ $(document).ready(function() {
     $('#portfolio-row-1').css('opacity', 1);*/
 
     portfolio2Shown = true;
-    $("#portfolio-row-2").css("opacity", 1);
+    $('#portfolio-row-2').css('opacity', 1);
 
     portfolio3Shown = true;
-    $("#portfolio-row-3").css("opacity", 1);
+    $('#portfolio-row-3').css('opacity', 1);
   }
 
 
-  $(window).scroll(function(e) {
+  $(window).scroll((event) => {
 
     // do not auto scroll for mobile
     if (smallDevice.matches) {
@@ -166,8 +158,8 @@ $(document).ready(function() {
 
     // if we are blocking scrolling, just do this
     if (!canScroll) {
-      e.preventDefault();
-      e.stopPropagation();
+      event.preventDefault();
+      event.stopPropagation();
       currentScroll = $(window).scrollTop();
       return false;
     }
@@ -178,37 +170,37 @@ $(document).ready(function() {
         $('#portfolio-row-1').animate({opacity: 1}, 1000);
     }*/
 
-    if (!portfolio2Shown && isElementInViewport($("#portfolio-row-2 img"))) {
+    if (!portfolio2Shown && isElementInViewport($('#portfolio-row-2 img'))) {
       portfolio2Shown = true;
-      $("#portfolio-row-2").animate({ opacity: 1 }, 1000);
+      $('#portfolio-row-2').animate({ opacity: 1 }, 1000);
     }
 
-    if (!portfolio3Shown && isElementInViewport($("#portfolio-row-3 img"))) {
+    if (!portfolio3Shown && isElementInViewport($('#portfolio-row-3 img'))) {
       portfolio3Shown = true;
-      $("#portfolio-row-3").animate({ opacity: 1 }, 1000);
+      $('#portfolio-row-3').animate({ opacity: 1 }, 1000);
     }
 
 
     // if user starts scrolling and we're at the top, auto scroll to the first section
     if ((canScroll) && (currentScroll === 0)) {
       canScroll = false;
-      $(".navbar").animate({ "padding-top": "0px" }, 500).css("background", "#0D0D0D").css("box-shadow", "0px 1px 2px #020202");
-      $("html, body").animate({
-        scrollTop: $("#profile").offset().top - $(".navbar").height(),
+      $('.navbar').animate({ 'padding-top': '0px' }, 500).css('background', '#0D0D0D').css('box-shadow', '0px 1px 2px #020202');
+      $('html, body').animate({
+        scrollTop: $('#profile').offset().top - $('.navbar').height(),
       }, 500, function() {
         canScroll = true;
       });
     }
     // if we are above the first section and scrolling up, auto scroll to top of page
-    else if ((canScroll) && (currentScroll > 0) && ($(window).scrollTop() < ($("#profile").offset().top - $(".navbar").height() - 150))) {
+    else if ((canScroll) && (currentScroll > 0) && ($(window).scrollTop() < ($('#profile').offset().top - $('.navbar').height() - 150))) {
       canScroll = false;
-      $(".navbar").animate({ "padding-top": "30px" }, 500);
-      $(".navbar").css("background", "none");
-      $(".navbar").css("box-shadow", "none");
-      $(".navbar").css("border-color", "transparent");
-      $("html, body").animate({
+      $('.navbar').animate({ 'padding-top': '30px' }, 500);
+      $('.navbar').css('background', 'none');
+      $('.navbar').css('box-shadow', 'none');
+      $('.navbar').css('border-color', 'transparent');
+      $('html, body').animate({
         scrollTop: 0,
-      }, 500, function() {
+      }, 500, () => {
         canScroll = true;
       });
     }
@@ -218,16 +210,16 @@ $(document).ready(function() {
 
 
   // if user clicks the logo, auto scroll to top of page
-  $(".navbar-brand").click(function(e) {
-    e.preventDefault();
+  $('.navbar-brand').click((event) => {
+    event.preventDefault();
     if (!(smallDevice.matches)) {
-      $(".navbar").animate({ "padding-top": "30px" }, 500);
-      $(".navbar").css("background", "none");
-      $(".navbar").css("box-shadow", "none");
-      $(".navbar").css("border-color", "transparent");
+      $('.navbar').animate({ 'padding-top': '30px' }, 500);
+      $('.navbar').css('background', 'none');
+      $('.navbar').css('box-shadow', 'none');
+      $('.navbar').css('border-color', 'transparent');
     }
     canScroll = false;
-    $("html, body").animate({
+    $('html, body').animate({
       scrollTop: 0,
     }, 500, function() {
       canScroll = true;
--- source/php/send.php
+++ source/php/send.php
@@ -1,14 +1,12 @@
 <?php
-
-	if($_POST){
-		$to = "ameer157@gmail.com";
-		$theName = $_POST['name'];
-		$subject = "You have been contacted via your website by $theName";
-	    $theEmail = $_POST['email'];
-	    $theMessage = $_POST['message'];
-	    $headers = "From:  $theEmail";
-	
-	   mail($to, $subject, $theMessage, $headers);
-	}
-
+if ($_POST)
+{
+    $to = "ameer157@gmail.com";
+    $sender = $_POST['name'];
+    $subject = "You have been contacted via your website by $sender";
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $headers = "From:  $email";
+    mail($to, $subject, $message, $headers);
+}
 ?>
