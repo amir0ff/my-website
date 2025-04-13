312619be51ecefb268d7c23f24700388c1a90108
Amir <ameer157@gmail.com>
2025-04-13 14:54:33 -0400

    Modernize gulp configuration file
    Signed-off-by: Amir <ameer157@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 import gulp from 'gulp';
-//import image from 'gulp-image';
 import terser from 'gulp-terser';
 import sizediff from 'gulp-sizediff';
 import preprocess from 'gulp-preprocess';
@@ -19,10 +18,10 @@ import * as sass from 'sass';
 const sassCompiler = gulpSass(sass);
 
 /**
- Parses build task arguments
- "$ gulp build --prod" for production build
- "$ gulp build" for development build
- **/
+ * Parses build task arguments
+ * "$ gulp build --prod" for production build
+ * "$ gulp build" for development build
+ */
 const args = minimist(process.argv.slice(2));
 
 const buildType = args.prod ? 'production' : 'development';
@@ -30,7 +29,7 @@ const prodMessage = 'Look at .github/workflows/node.js.yml for deployment build 
 const devMessage = 'File changes will be watched and trigger a page reload.';
 const buildMessage = buildType === 'production' ? prodMessage : devMessage;
 
-// Main build directories
+//   Main build directories
 const sourceDir = './source/';
 const buildDir = './build/';
 
@@ -52,10 +51,7 @@ const css = {
   sassOpts: {
     outputStyle: 'compressed',
   },
-  plugins: [
-    autoprefixer(),
-    cssnano(),
-  ],
+  plugins: [autoprefixer(), cssnano()],
 };
 
 const js = {
@@ -89,134 +85,165 @@ const html = {
   },
 };
 
-// HTML Compression
-gulp.task('html', () => {
-  let page = gulp.src(html.sourceDir).pipe(preprocess({
-    context: html.context,
-  }));
-  if (buildType === 'development') {
-    return page.pipe(gulp.dest(html.buildDir));
-  } else {
-    return page
+//   Clear build directory
+export const beforeScript = async () => {
+  await deleteAsync([buildDir + '/**/*']);
+};
+beforeScript.displayName = 'beforeScript'; //   Explicitly name the task
+
+//   HTML Compression
+export const htmlTask = async () => {
+  try {
+    let page = gulp.src(html.sourceDir).pipe(
+      preprocess({
+        context: html.context,
+      })
+    );
+    if (buildType === 'development') {
+      await page.pipe(gulp.dest(html.buildDir));
+    } else {
+      await page
+        .pipe(sizediff.start())
+        .pipe(htmlclean())
+        .pipe(
+          sizediff.stop({
+            title: 'HTML Minification',
+          })
+        )
+        .pipe(gulp.dest(html.buildDir));
+    }
+  } catch (error) {
+    console.error('Error during html task:', error);
+    throw error; //   Re-throw the error to fail the task
+  }
+};
+htmlTask.displayName = 'html';
+
+//   Images Compression
+export const imagesTask = async () => {
+  try {
+    const imagemin = (await import('gulp-imagemin')).default; //   Important: Access the default export
+    await gulp
+      .src(images.sourceDir, { encoding: false })
       .pipe(sizediff.start())
-      .pipe(htmlclean())
-      .pipe(sizediff.stop({
-        title: 'HTML Minification',
-      }))
-      .pipe(gulp.dest(html.buildDir));
+      .pipe(imagemin())
+      .pipe(
+        sizediff.stop({
+          title: 'Images Compression',
+        })
+      )
+      .pipe(gulp.dest(images.buildDir));
+  } catch (error) {
+    console.error('Error during images task:', error);
+    throw error;
   }
-});
+};
+imagesTask.displayName = 'images';
 
-// Images Compression
-gulp.task('images', () => {
-  return gulp.src(images.sourceDir, { encoding: false })
-    .pipe(sizediff.start())
-    //.pipe(image())
-    .pipe(sizediff.stop({
-      title: 'Images Compression',
-    }))
-    .pipe(gulp.dest(images.buildDir));
-});
+//   Build CSS
+export const sassTask = async () => {
+  try {
+    const postcssPlugins = [];
+    if (buildType === 'production') {
+      postcssPlugins.push(...css.plugins);
+    }
 
-// Build CSS
-gulp.task('sass', () => {
-  const postcssPlugins = [];
-  if (buildType === 'production') {
-    postcssPlugins.push(...css.plugins);
-  }
-
-  return gulp.src(css.sourceDir)
-    .pipe(sizediff.start())
-    .pipe(sassCompiler(css.sassOpts).on('error', sassCompiler.logError))
-    .pipe(postcss(postcssPlugins))
-    .pipe(concat('main.min.css'))
-    .pipe(sizediff.stop({
-      title: 'CSS Compression',
-    }))
-    .pipe(gulp.dest(css.buildDir));
-});
-
-
-// Just copy all php module files to build folder
-gulp.task('php', () => {
-  return gulp.src(php.sourceDir).pipe(gulp.dest(php.buildDir));
-});
-
-// Build JavaScript
-gulp.task('js', () => {
-  if (buildType === 'development') {
-    return gulp.src(
-      [
-        npm_modules.jquery,
-        npm_modules.bootstrap,
-        npm_modules.moment,
-        js.sourceDir,
-      ])
-      .pipe(concat('main.js'))
-      .pipe(gulp.dest(js.buildDir));
-  } else {
-    return gulp.src(
-      [
-        npm_modules.jquery,
-        npm_modules.bootstrap,
-        npm_modules.moment,
-        js.sourceDir,
-      ])
-      .pipe(concat('main.min.js'))
+    await gulp
+      .src(css.sourceDir)
       .pipe(sizediff.start())
-      .pipe(stripdebug())
-      .pipe(terser({ format: { comments: false } }))
-      .pipe(sizediff.stop({
-        title: 'JavaScript Compression',
-      }))
-      .pipe(gulp.dest(js.buildDir));
+      .pipe(sassCompiler(css.sassOpts).on('error', sassCompiler.logError))
+      .pipe(postcss(postcssPlugins))
+      .pipe(concat('main.min.css'))
+      .pipe(
+        sizediff.stop({
+          title: 'CSS Compression',
+        })
+      )
+      .pipe(gulp.dest(css.buildDir));
+  } catch (error) {
+    console.error('Error during sass task:', error);
+    throw error;
   }
-});
+};
+sassTask.displayName = 'sass';
 
-gulp.task('browserSyncReload', (done) => {
+//   Just copy all php module files to build folder
+export const phpTask = async () => {
+  try {
+    await gulp.src(php.sourceDir).pipe(gulp.dest(php.buildDir));
+  } catch (error) {
+    console.error('Error during php task:', error);
+    throw error;
+  }
+};
+phpTask.displayName = 'php';
+
+//   Build JavaScript
+export const jsTask = async () => {
+  try {
+    if (buildType === 'development') {
+      await gulp
+        .src([npm_modules.jquery, npm_modules.bootstrap, npm_modules.moment, js.sourceDir])
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest(js.buildDir));
+    } else {
+      await gulp
+        .src([npm_modules.jquery, npm_modules.bootstrap, npm_modules.moment, js.sourceDir])
+        .pipe(concat('main.min.js'))
+        .pipe(sizediff.start())
+        .pipe(stripdebug())
+        .pipe(terser({ format: { comments: false } }))
+        .pipe(
+          sizediff.stop({
+            title: 'JavaScript Compression',
+          })
+        )
+        .pipe(gulp.dest(js.buildDir));
+    }
+  } catch (error) {
+    console.error('Error during js task:', error);
+    throw error;
+  }
+};
+jsTask.displayName = 'js';
+
+export const browserSyncReload = (done) => {
   browserSync.reload();
   done();
-});
+};
+browserSyncReload.displayName = 'browserSyncReload';
 
-// Clear build directory
-gulp.task('beforeScript', async (done) => {
-  await deleteAsync([
-    buildDir + '/**/*',
-  ]);
-  done();
-});
-
-// Runs after all gulp.js build scripts are done
-gulp.task('afterScript', (done) => {
+//   Runs after all gulp.js build scripts are done
+export const afterScript = (done) => {
   if (buildType === 'development') {
     browserSync(browserSyncConfig);
-    gulp.watch(html.watchList, gulp.series('html', 'browserSyncReload'));
-    gulp.watch(images.watchList, gulp.series('images', 'browserSyncReload'));
-    gulp.watch(css.watchList, gulp.series('sass', 'browserSyncReload'));
-    gulp.watch(js.watchList, gulp.series('js', 'browserSyncReload'));
-    gulp.watch(php.watchList, gulp.series('php', 'browserSyncReload'));
+    gulp.watch(html.watchList, gulp.series(htmlTask, browserSyncReload));
+    gulp.watch(images.watchList, gulp.series(imagesTask, browserSyncReload));
+    gulp.watch(css.watchList, gulp.series(sassTask, browserSyncReload));
+    gulp.watch(js.watchList, gulp.series(jsTask, browserSyncReload));
+    gulp.watch(php.watchList, gulp.series(phpTask, browserSyncReload));
   }
   ncu.run({
     packageFile: 'package.json',
-  })
-    .then((upgraded) => {
-      console.log('--- End of build ---');
-      console.log('');
-      console.log('This was a ' + buildType + ' build.');
-      console.log('');
-      console.log('Files are generated in the ' + buildDir + ' directory.');
-      console.log(buildMessage);
-      if (Object.keys(upgraded).length === 0) {
-        console.log('All npm dependencies are up to date!');
-      } else {
-        console.log('The following npm dependencies have updates');
-        console.log('To update npm dependencies, run the following command in your terminal:, `ncu -u`', upgraded);
-      }
-    });
+  }).then((upgraded) => {
+    console.log('--- End of build ---');
+    console.log('');
+    console.log('This was a ' + buildType + ' build.');
+    console.log('');
+    console.log('Files are generated in the ' + buildDir + ' directory.');
+    console.log(buildMessage);
+    if (Object.keys(upgraded).length === 0) {
+      console.log('All npm dependencies are up to date!');
+    } else {
+      console.log('The following npm dependencies have updates');
+      console.log('To update npm dependencies, run the following command in your terminal:, `ncu -u`', upgraded);
+    }
+  });
   done();
-});
+};
+afterScript.displayName = 'afterScript';
 
-// Main build task
-gulp.task('build', gulp.series('beforeScript', 'html', 'images', 'sass', 'js', 'php', 'afterScript'), (done) => {
-  done();
-});
+//   Main build task
+export const build = gulp.series(beforeScript, htmlTask, imagesTask, sassTask, jsTask, phpTask, afterScript);
+
+export default build;
