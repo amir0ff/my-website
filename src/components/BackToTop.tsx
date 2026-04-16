2ae4c9bd525b1882aa939ad8fcf07447764f557f
Amir <ameer157@gmail.com>
2026-04-16 18:18:40 -0400

    refactor: remove Next.js artifacts and clean up dead dependencies
    - Remove 'use client' directives from all components (no-op in Vite)
    
    - Remove .next/out from ESLint ignores
    
    - Drop unused framer-motion and moment.js dependencies
    
    - Migrate @studio-freight/lenis to lenis (v1.0.42 -> v1.3.23)

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 
 import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,7 +1,5 @@
-"use client";
-
 import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 
 export default function SmoothScroll({
   children,
